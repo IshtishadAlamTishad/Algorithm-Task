872f062b792b033d38ead010748428b0c3e16068
Ishtishad Alam Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-03-02 00:04:45 +0600

    Create test.cpp

---
+++ LabExm/test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int middleID = 46130;
+    cout << (middleID%3+1);
+}
